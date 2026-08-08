6f6d57aeae4bd9f6cecc35bcd63ca42c5cc502b4
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:47:59 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 2.3.x branch

--- src/test/java/com/google/code/kaptcha/spring/boot/KaptchaResolverTest.java
+++ src/test/java/com/google/code/kaptcha/spring/boot/KaptchaResolverTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -13,24 +13,26 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-/**
- * 
- */
-package com.google.code.kaptcha.spring.boot.ext.exception;
+package com.google.code.kaptcha.spring.boot;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * 验证码未找到异常
+ * Unit tests for {{ @link KaptchaResolver }}.
+ *
  * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
  */
-@SuppressWarnings("serial")
-public class CaptchaNotFoundException extends Exception {
-	
-	public CaptchaNotFoundException() {
-		super("no comparable captcha found");
-    }
+@DisplayName("KaptchaResolver Tests")
+class KaptchaResolverTest {
 
-    public CaptchaNotFoundException(String message) {
-        super(message);
+    @Test
+    @DisplayName("Instance can be created via constructor")
+    void testInstantiation() {
+        KaptchaResolver instance = new KaptchaResolver();
+        assertThat(instance).isNotNull();
     }
-	
 }
